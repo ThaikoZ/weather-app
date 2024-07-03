2c465aaaed4d3177fceb75bcde40d698471b186c
Adrian Sudak <adriansudak@outlook.com>
2024-07-03 19:28:47 +0200

    Add sidebar collapse on background click

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -21,31 +21,45 @@ const sidebarMenu = [
 ];
 
 const Sidebar = () => {
-  const { isCollapsed } = useSidebarContext();
+  const { isCollapsed, toggleCollapse } = useSidebarContext();
+
   return (
-    <div className="flex top-0 left-0 h-full fixed">
-      <div
-        className={classNames(
-          { "w-20": isCollapsed },
-          { "w-56": !isCollapsed },
-          "flex flex-col gap-10  justify-between backdrop-blur-md bg-black bg-opacity-20 shadow-lg px-5 py-8 text-center text-white transition-transform duration-500"
-        )}
-      >
-        <div className="flex flex-col gap-10">
-          <div className="flex items-center gap-2.5 font-semibold justify-start p-1">
-            <CloudIcon className="size-8" />
-            {!isCollapsed && <h1 className="text-xl">Weather App</h1>}
+    <>
+      <div className="flex top-0 left-0 h-full fixed z-30 ">
+        <div
+          className={classNames(
+            { "w-20": isCollapsed },
+            { "w-56": !isCollapsed },
+            "flex flex-col gap-10  justify-between backdrop-blur-md bg-black bg-opacity-20 shadow-lg px-5 py-8 text-center text-white transition-transform duration-500"
+          )}
+        >
+          <div className="flex flex-col gap-10">
+            <div className="flex items-center gap-2.5 font-semibold justify-start p-1">
+              <CloudIcon className="size-8" />
+              {!isCollapsed && <h1 className="text-xl">Weather App</h1>}
+            </div>
+            <SidebarMenu menuItems={sidebarMenu} />
           </div>
-          <SidebarMenu menuItems={sidebarMenu} />
-        </div>
-        <div className="flex justify-end">
-          <ToggleCollapseButton />
+          <div className="flex justify-end">
+            <ToggleCollapseButton />
+          </div>
         </div>
       </div>
-      <div className="p-8">
+      {!isCollapsed && (
+        <div className="flex absolute top-0 w-full overflow-hidden h-full z-10 cursor-pointer">
+          <div className="min-w-56 max-w-56"></div>
+          <div
+            className="w-full bg-black bg-opacity-55 "
+            onClick={toggleCollapse}
+          >
+            d
+          </div>
+        </div>
+      )}
+      <div className="ps-20">
         <Outlet />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,27 @@
 const DashboardPage = () => {
-  return <div>gowno</div>;
+  return (
+    <div className="flex flex-col gap-10 w-full p-10">
+      <div className="flex justify-between w-full">
+        <div className="bg-white">Search Bar</div>
+        <div className="bg-white">Notify + Avatar</div>
+      </div>
+      <div className="w-full grid grid-cols-12 gap-5 space-5">
+        <div className="col-span-12 sm:col-span-6 lg:col-span-3 bg-rose-500">
+          Current Weather
+        </div>
+        <div className="col-span-12 sm:col-span-6 lg:col-span-6 bg-rose-500">
+          Map
+        </div>
+        <div className="col-span-12 sm:col-span-6 lg:col-span-3 bg-rose-500">
+          Popular Cities
+        </div>
+        <div className="col-span-12 sm:col-span-6 lg:col-span-3 bg-rose-500">
+          Forecast
+        </div>
+        <div className="col-span-12 lg:col-span-9 bg-rose-500">Summary</div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
